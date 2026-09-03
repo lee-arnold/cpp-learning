76d3166dc4da321c619d9135fbd5bc23131ba68a
Lee Arnold <lee@MacBook-Pro.local>
2026-09-03 14:46:43 +0100

    add functions

--- src/multiply-by-two.cpp
+++ src/multiply-by-two.cpp
@@ -1,11 +1,21 @@
 #include "./multiply-by-two.hpp"
 #include <iostream>
 
-void multiply_by_two() {
+int get_value_from_user() {
     std::cout << "Enter an integer: " << std::endl;
 
     int num{};
     std::cin >> num;
 
+    return num;
+}
+
+void print_double(int num) {
     std::cout << "Double that number is: " << num * 2 << std::endl;
+}
+
+void multiply_by_two() {
+    int num{get_value_from_user()};
+
+    print_double(num);
 }
