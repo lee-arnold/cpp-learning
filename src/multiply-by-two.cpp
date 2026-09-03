6a2959d3362180b363d1560f0d135ff62fd6584b
Lee Arnold <lee@MacBook-Pro.local>
2026-09-03 14:38:02 +0100

    change to void

--- src/multiply-by-two.cpp
+++ src/multiply-by-two.cpp
@@ -1,13 +1,11 @@
 #include "./multiply-by-two.hpp"
 #include <iostream>
 
-int multiply_by_two() {
+void multiply_by_two() {
     std::cout << "Enter an integer: " << std::endl;
 
     int num{};
     std::cin >> num;
 
     std::cout << "Double that number is: " << num * 2 << std::endl;
-
-    return 0;
 }
--- src/multiply-by-two.hpp
+++ src/multiply-by-two.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-int multiply_by_two();
+void multiply_by_two();
