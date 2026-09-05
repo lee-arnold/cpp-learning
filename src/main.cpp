2e11b561f23780c09601ab1fd21c070430221121
Lee Arnold <lee@MacBook-Pro.local>
2026-09-05 23:35:24 +0100

    tidy up directories

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-// #include "./iostream.hpp"
-#include "./multiply-by-two.hpp"
+// #include "./lessons/iostream.hpp"
+#include "./lessons/multiply-by-two.hpp"
 
 int main() {
     multiply_by_two();
