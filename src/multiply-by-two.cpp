bf3938e62b93d252610625168a8f3355b2c6bb1d
Lee Arnold <lee@MacBook-Pro.local>
2026-09-05 23:22:10 +0100

    add this comment

--- src/multiply-by-two.cpp
+++ src/multiply-by-two.cpp
@@ -4,6 +4,7 @@
 int get_value_from_user() {
     std::cout << "Enter an integer: " << std::endl;
 
+    // it's best practice to define variables closest to their first use, within reason
     int num{};
     std::cin >> num;
 
@@ -18,4 +19,5 @@ void multiply_by_two() {
     int num{get_value_from_user()};
 
     print_double(num);
+    // alternatively print_double(get_value_from_user());
 }
