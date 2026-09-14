cb12c8fab7c036f8f72eda7b3e5a3f299a0d2171
Lee Arnold <lee@MacBook-Pro.local>
2026-09-15 00:34:09 +0100

    execute a simple move manually

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "chess/board.h"
+#include "chess/moves/normal.h"
+#include "chess/position.h"
 
 int main() {
     chess::Board board{};
+
+    auto move{chess::Normal{chess::Position{1, 1}, chess::Position{3, 1}}};
+
+    move.execute(board);
+
     board.print();
 
     return 0;
