10294c605d6935ccdb9cb9ac2c86a49111947a19
Lee Arnold <lee@MacBook-Pro.local>
2026-09-16 20:45:08 +0100

    delete old lessons files

--- src/lessons/chapter-quizzes/five.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <string_view>
-
-std::string get_name(int num);
-int get_age(std::string_view string_view);
-void run_quiz_5_question_4();
--- src/lessons/print/print-double.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void print_double(int num) {
-    std::cout << "Double that number is: " << num * 2 << std::endl;
-}
